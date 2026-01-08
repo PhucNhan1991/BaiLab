9ffc11998689f920ba7da256986cad9d3a8bc8c3
PhucNhan1991 <phucnhan27081991@gmail.com>
2026-01-08 12:10:07 +0700

    First Commit

--- Lab2/Bai1.c
+++ Lab2/Bai1.c
@@ -4,8 +4,8 @@ int main()
     float x, y; //Khai bao 2 bien x, y
     printf("Nhap 2 so x va y\n");
     scanf("%f %f",&x, &y);
-    printf("Tong 2 so x + y = %.1f\n", x+y);
-    printf("Hieu 2 so x - y = %.1f", x-y);
+    printf("Tong 2 so x + y = %.1f\n", x+y); // In ra màn hình tổng của 2 số x +y
+    printf("Hieu 2 so x - y = %.1f", x-y); // In ra màn hình hiệu của 2 số x -y
     return 0;
 
 }
